77cf202826ad4ff955a9ce6a280ba474053b9bf6
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-08-15 11:23:48 +0200

    Fixed travis-deploy.sh

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-echo $TRAVIS_PULL_REQUEST
-echo $TRAVIS_TAG
-test "${TRAVIS_PULL_REQUEST}" == "false" && test "${TRAVIS_TAG}" != "" && mvn deploy --settings travis-settings.xml
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_TAG}" != "" ]; then
+  mvn deploy --settings travis-settings.xml
+fi
