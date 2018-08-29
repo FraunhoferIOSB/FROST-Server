05821076f7998fda5751a9b54139d5b01a081ab2
phertweck <philipp.hertweck@iosb.fraunhofer.de>
2018-08-29 15:14:57 +0100

    [Helm] Added snapshot repository

--- helm/build.sh
+++ helm/build.sh
@@ -4,10 +4,21 @@ set -e
 git config --global user.email "noReply@local"
 git config --global user.name "Travis Build"
 
-git clone --quiet --branch master https://github.com/FraunhoferIOSB/helm-charts.git
+# release version
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_TAG}" != "" ]; then
+  echo "Building release helm chart"
+  git clone --quiet --branch master https://github.com/FraunhoferIOSB/helm-charts.git
+  /tmp/helm lint ./helm/frost-server
+  /tmp/helm package ./helm/frost-server -d ./helm-charts
+  /tmp/helm repo index --url https://fraunhoferiosb.github.io/helm-charts/ ./helm-charts
+else
+  echo "Building snapshot helm chart"
+  git clone --quiet --branch master https://github.com/FraunhoferIOSB/helm-charts-snapshots.git
+  /tmp/helm lint ./helm/frost-server
+  /tmp/helm package ./helm/frost-server -d ./helm-charts-snapshots
+  /tmp/helm repo index --url https://fraunhoferiosb.github.io/helm-charts-snapshots/ ./helm-charts-snapshots
+fi
 
-/tmp/helm lint ./helm/frost-server
-/tmp/helm package ./helm/frost-server -d ./helm-charts
-/tmp/helm repo index --url https://fraunhoferiosb.github.io/helm-charts/ ./helm-charts
+
 
 echo "Helm chart build"
--- helm/upload.sh
+++ helm/upload.sh
@@ -6,7 +6,14 @@ set -e
 cd helm-charts
 git add .
 git remote rm origin
-git remote add origin https://phertweck:$GITHUB_API_KEY@github.com/FraunhoferIOSB/helm-charts
+
+# release version
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_TAG}" != "" ]; then
+  git remote add origin https://phertweck:$GITHUB_API_KEY@github.com/FraunhoferIOSB/helm-charts
+else
+  git remote add origin https://phertweck:$GITHUB_API_KEY@github.com/FraunhoferIOSB/helm-charts-snapshots
+fi
+
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed"
 git push origin master -fq
 cd ../
