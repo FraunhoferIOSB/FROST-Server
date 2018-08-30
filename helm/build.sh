da9595b5d47431cf2ee717875ae2c6a91ca2298e
phertweck <philipp.hertweck@iosb.fraunhofer.de>
2018-08-30 13:41:32 +0200

    [Helm] Fixed URL

--- helm/build.sh
+++ helm/build.sh
@@ -17,6 +17,6 @@ echo "Building snapshot helm chart"
 git clone --quiet --branch master https://github.com/FraunhoferIOSB/helm-charts-snapshot.git
 /tmp/helm lint ./helm/frost-server
 /tmp/helm package ./helm/frost-server -d ./helm-charts-snapshot
-/tmp/helm repo index --url https://fraunhoferiosb.github.io/helm-charts-snapshots/ ./helm-charts-snapshot
+/tmp/helm repo index --url https://fraunhoferiosb.github.io/helm-charts-snapshot/ ./helm-charts-snapshot
 
 echo "Helm chart build"
