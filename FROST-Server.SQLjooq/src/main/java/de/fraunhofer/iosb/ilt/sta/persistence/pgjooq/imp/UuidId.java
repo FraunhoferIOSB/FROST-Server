d8871e7399c1919813d36f49b2d361482af36f3a
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2019-02-08 16:27:34 +0100

    Added UuidId support for jooq backend

---
+++ FROST-Server.SQLjooq/src/main/java/de/fraunhofer/iosb/ilt/sta/persistence/pgjooq/imp/UuidId.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2016 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
+ * Karlsruhe, Germany.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.fraunhofer.iosb.ilt.sta.persistence.pgjooq.imp;
+
+import de.fraunhofer.iosb.ilt.sta.model.core.Id;
+import de.fraunhofer.iosb.ilt.sta.persistence.BasicPersistenceType;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ *
+ * @author scf
+ */
+public class UuidId implements Id {
+
+    private UUID value;
+
+    public UuidId(UUID value) {
+        this.value = value;
+    }
+
+    public UuidId(String value) {
+        this.value = UUID.fromString(value);
+    }
+
+    @Override
+    public BasicPersistenceType getBasicPersistenceType() {
+        return BasicPersistenceType.BYTEARRAY;
+    }
+
+    @Override
+    public Object asBasicPersistenceType() {
+        return value;
+    }
+
+    @Override
+    public void fromBasicPersitenceType(Object data) {
+        if (data instanceof UUID) {
+            value = (UUID) data;
+        } else {
+            value = UUID.fromString(data.toString());
+        }
+    }
+
+    @Override
+    public UUID getValue() {
+        return value;
+    }
+
+    @Override
+    public String getUrl() {
+        return "'" + value.toString() + "'";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UuidId other = (UuidId) obj;
+        return Objects.equals(this.value, other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toString(getValue());
+    }
+
+}
---
+++ FROST-Server.SQLjooq/src/main/java/de/fraunhofer/iosb/ilt/sta/persistence/pgjooq/tables/uuidid/TableUuidMultiDatastreamsObsProperties.java
@@ -0,0 +1,94 @@
+package de.fraunhofer.iosb.ilt.sta.persistence.pgjooq.tables.uuidid;
+
+import de.fraunhofer.iosb.ilt.sta.persistence.pgjooq.tables.AbstractTableMultiDatastreamsObsProperties;
+import java.util.UUID;
+import org.jooq.Name;
+import org.jooq.Record;
+import org.jooq.TableField;
+import org.jooq.impl.DSL;
+
+public class TableUuidMultiDatastreamsObsProperties extends AbstractTableMultiDatastreamsObsProperties<UUID> {
+
+    private static final long serialVersionUID = 344714892;
+
+    /**
+     * The reference instance of
+     * <code>public.MULTI_DATASTREAMS_OBS_PROPERTIES</code>
+     */
+    public static final TableUuidMultiDatastreamsObsProperties MULTI_DATASTREAMS_OBS_PROPERTIES = new TableUuidMultiDatastreamsObsProperties();
+
+    /**
+     * @return The class holding records for this type
+     */
+    @Override
+    public Class<Record> getRecordType() {
+        return Record.class;
+    }
+
+    @Override
+    public TableField<Record, UUID> getMultiDatastreamId() {
+        return multiDatastreamId;
+    }
+
+    @Override
+    public TableField<Record, UUID> getObsPropertyId() {
+        return ObsPropertyId;
+    }
+
+    /**
+     * The column
+     * <code>public.MULTI_DATASTREAMS_OBS_PROPERTIES.MULTI_DATASTREAM_ID</code>.
+     */
+    public final TableField<Record, UUID> multiDatastreamId = createField("MULTI_DATASTREAM_ID", org.jooq.impl.SQLDataType.UUID.nullable(false), this, "");
+
+    /**
+     * The column
+     * <code>public.MULTI_DATASTREAMS_OBS_PROPERTIES.OBS_PROPERTY_ID</code>.
+     */
+    public final TableField<Record, UUID> ObsPropertyId = createField("OBS_PROPERTY_ID", org.jooq.impl.SQLDataType.UUID.nullable(false), this, "");
+
+    /**
+     * Create a <code>public.MULTI_DATASTREAMS_OBS_PROPERTIES</code> table
+     * reference
+     */
+    public TableUuidMultiDatastreamsObsProperties() {
+        super();
+    }
+
+    /**
+     * Create an aliased <code>public.MULTI_DATASTREAMS_OBS_PROPERTIES</code>
+     * table reference
+     */
+    public TableUuidMultiDatastreamsObsProperties(String alias) {
+        this(DSL.name(alias), MULTI_DATASTREAMS_OBS_PROPERTIES);
+    }
+
+    /**
+     * Create an aliased <code>public.MULTI_DATASTREAMS_OBS_PROPERTIES</code>
+     * table reference
+     */
+    public TableUuidMultiDatastreamsObsProperties(Name alias) {
+        this(alias, MULTI_DATASTREAMS_OBS_PROPERTIES);
+    }
+
+    private TableUuidMultiDatastreamsObsProperties(Name alias, TableUuidMultiDatastreamsObsProperties aliased) {
+        super(alias, aliased);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TableUuidMultiDatastreamsObsProperties as(String alias) {
+        return new TableUuidMultiDatastreamsObsProperties(DSL.name(alias), this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TableUuidMultiDatastreamsObsProperties as(Name alias) {
+        return new TableUuidMultiDatastreamsObsProperties(alias, this);
+    }
+
+}
