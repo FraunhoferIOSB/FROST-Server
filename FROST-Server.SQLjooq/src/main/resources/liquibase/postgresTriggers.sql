e68f1b3341494c0dc8d4d1ab1441d2cd6d65557e
Hylke van der Schaaf <hylke.vds@gmail.com>
2021-04-04 00:04:10 +0200

    Improved performance of Datastream update triggers
    The triggers would execute multiple separate update statements, one for
    each property. This was very inefficient. Now the different updates are
    gathered and send to the database in one update statement.
    Also, the resultTime would be updated if null, even if the new value was
    also null. Now resultTime is only updated if the value actually changes.

--- FROST-Server.SQLjooq/src/main/resources/liquibase/postgresTriggers.sql
+++ FROST-Server.SQLjooq/src/main/resources/liquibase/postgresTriggers.sql
@@ -64,48 +64,77 @@ create or replace function datastreams_update_insert()
   returns trigger as
 $BODY$
 declare
-"DS_ROW" "DATASTREAMS"%rowtype;
-"MDS_ROW" "MULTI_DATASTREAMS"%rowtype;
+	"DS_ROW" RECORD;
+	"MDS_ROW" RECORD;
+	queryset TEXT := '';
+	delimitr char(1) := ' ';
 begin
 
 if (NEW."DATASTREAM_ID" is not null) 
 then 
-	select * into "DS_ROW" from "DATASTREAMS" where "DATASTREAMS"."ID"=NEW."DATASTREAM_ID";
+	select "ID","PHENOMENON_TIME_START","PHENOMENON_TIME_END","RESULT_TIME_START","RESULT_TIME_END","OBSERVED_AREA"
+	    into "DS_ROW" from "DATASTREAMS" where "DATASTREAMS"."ID"=NEW."DATASTREAM_ID";
 	if (NEW."PHENOMENON_TIME_START"<"DS_ROW"."PHENOMENON_TIME_START" or "DS_ROW"."PHENOMENON_TIME_START" is null) then
-		update "DATASTREAMS" set "PHENOMENON_TIME_START" = NEW."PHENOMENON_TIME_START" where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = $1."PHENOMENON_TIME_START"';
+		delimitr := ',';
 	end if;
 	if (coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") > "DS_ROW"."PHENOMENON_TIME_END" or "DS_ROW"."PHENOMENON_TIME_END" is null) then
-		update "DATASTREAMS" set "PHENOMENON_TIME_END" = coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = coalesce($1."PHENOMENON_TIME_END", $1."PHENOMENON_TIME_START")';
+		delimitr := ',';
 	end if;
 
-	if (NEW."RESULT_TIME"<"DS_ROW"."RESULT_TIME_START" or "DS_ROW"."RESULT_TIME_START" is null) then
-		update "DATASTREAMS" set "RESULT_TIME_START" = NEW."RESULT_TIME" where "DATASTREAMS"."ID" = "DS_ROW"."ID";
-	end if;
-	if (NEW."RESULT_TIME" > "DS_ROW"."RESULT_TIME_END" or "DS_ROW"."RESULT_TIME_END" is null) then
-		update "DATASTREAMS" set "RESULT_TIME_END" = NEW."RESULT_TIME" where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+	if (NEW."RESULT_TIME" is not null) then
+		if (NEW."RESULT_TIME"<"DS_ROW"."RESULT_TIME_START" or "DS_ROW"."RESULT_TIME_START" is null) then
+			queryset := queryset || delimitr || '"RESULT_TIME_START" = $1."RESULT_TIME"';
+			delimitr := ',';
+		end if;
+		if (NEW."RESULT_TIME" > "DS_ROW"."RESULT_TIME_END" or "DS_ROW"."RESULT_TIME_END" is null) then
+			queryset := queryset || delimitr || '"RESULT_TIME_END" = $1."RESULT_TIME"';
+			delimitr := ',';
+		end if;
 	end if;
 
-	update "DATASTREAMS" SET "OBSERVED_AREA" = ST_ConvexHull(ST_Collect("OBSERVED_AREA", (select "GEOM" from "FEATURES" where "ID"=NEW."FEATURE_ID"))) where "DATASTREAMS"."ID"=NEW."DATASTREAM_ID";
+	if ("DS_ROW"."OBSERVED_AREA" is null or not ST_Within("DS_ROW"."OBSERVED_AREA",(select "GEOM" from "FEATURES" where "ID"=NEW."FEATURE_ID"))) then
+		queryset := queryset || delimitr || '"OBSERVED_AREA" = ST_ConvexHull(ST_Collect("OBSERVED_AREA", (select "GEOM" from "FEATURES" where "ID"=$1."FEATURE_ID")))';
+		delimitr := ',';
+	end if;
+	if (delimitr = ',') then
+		EXECUTE 'update "DATASTREAMS" SET ' || queryset ||  ' where "DATASTREAMS"."ID"=$1."DATASTREAM_ID"' using NEW;
+	end if;
+	return new;
 end if;
 
 if (NEW."MULTI_DATASTREAM_ID" is not null) 
 then 
-	select * into "MDS_ROW" from "MULTI_DATASTREAMS" where "MULTI_DATASTREAMS"."ID"=NEW."MULTI_DATASTREAM_ID";
+	select "ID","PHENOMENON_TIME_START","PHENOMENON_TIME_END","RESULT_TIME_START","RESULT_TIME_END","OBSERVED_AREA"
+	    into "MDS_ROW" from "MULTI_DATASTREAMS" where "MULTI_DATASTREAMS"."ID"=NEW."MULTI_DATASTREAM_ID";
 	if (NEW."PHENOMENON_TIME_START"<"MDS_ROW"."PHENOMENON_TIME_START" or "MDS_ROW"."PHENOMENON_TIME_START" is null) then
-		update "MULTI_DATASTREAMS" set "PHENOMENON_TIME_START" = NEW."PHENOMENON_TIME_START" where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = $1."PHENOMENON_TIME_START"';
+		delimitr := ',';
 	end if;
 	if (coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") > "MDS_ROW"."PHENOMENON_TIME_END" or "MDS_ROW"."PHENOMENON_TIME_END" is null) then
-		update "MULTI_DATASTREAMS" set "PHENOMENON_TIME_END" = coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = coalesce($1."PHENOMENON_TIME_END", $1."PHENOMENON_TIME_START")';
+		delimitr := ',';
 	end if;
 
-	if (NEW."RESULT_TIME"<"MDS_ROW"."RESULT_TIME_START" or "MDS_ROW"."RESULT_TIME_START" is null) then
-		update "MULTI_DATASTREAMS" set "RESULT_TIME_START" = NEW."RESULT_TIME" where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
-	end if;
-	if (NEW."RESULT_TIME" > "MDS_ROW"."RESULT_TIME_END" or "MDS_ROW"."RESULT_TIME_END" is null) then
-		update "MULTI_DATASTREAMS" set "RESULT_TIME_END" = NEW."RESULT_TIME" where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+	if (NEW."RESULT_TIME" is not null) then
+		if (NEW."RESULT_TIME"<"MDS_ROW"."RESULT_TIME_START" or "MDS_ROW"."RESULT_TIME_START" is null) then
+			queryset := queryset || delimitr || '"RESULT_TIME_START" = $1."RESULT_TIME"';
+			delimitr := ',';
+		end if;
+		if (NEW."RESULT_TIME" > "MDS_ROW"."RESULT_TIME_END" or "MDS_ROW"."RESULT_TIME_END" is null) then
+			queryset := queryset || delimitr || '"RESULT_TIME_END" = $1."RESULT_TIME"';
+			delimitr := ',';
+		end if;
 	end if;
 
-	update "MULTI_DATASTREAMS" SET "OBSERVED_AREA" = ST_ConvexHull(ST_Collect("OBSERVED_AREA", (select "GEOM" from "FEATURES" where "ID"=NEW."FEATURE_ID"))) where "MULTI_DATASTREAMS"."ID"=NEW."MULTI_DATASTREAM_ID";
+	if ("MDS_ROW"."OBSERVED_AREA" is null or not ST_Within("MDS_ROW"."OBSERVED_AREA",(select "GEOM" from "FEATURES" where "ID"=NEW."FEATURE_ID"))) then
+		queryset := queryset || delimitr || '"OBSERVED_AREA" = ST_ConvexHull(ST_Collect("OBSERVED_AREA", (select "GEOM" from "FEATURES" where "ID"=$1."FEATURE_ID")))';
+		delimitr := ',';
+	end if;
+	if (delimitr = ',') then
+		EXECUTE 'update "MULTI_DATASTREAMS" SET ' || queryset ||  ' where "MULTI_DATASTREAMS"."ID"=$1."MULTI_DATASTREAM_ID"' using NEW;
+	end if;
 end if;
 
 return new;
@@ -141,8 +170,10 @@ create or replace function datastreams_update_update()
   returns trigger as
 $BODY$
 declare
-"DS_ROW" "DATASTREAMS"%rowtype;
-"MDS_ROW" "MULTI_DATASTREAMS"%rowtype;
+	"DS_ROW" "DATASTREAMS"%rowtype;
+	"MDS_ROW" "MULTI_DATASTREAMS"%rowtype;
+	queryset TEXT := '';
+	delimitr char(1) := ' ';
 begin
 
 if (NEW."DATASTREAM_ID" is not null) 
@@ -151,24 +182,20 @@ then
 		for "DS_ROW" in select * from "DATASTREAMS" where "ID"=NEW."DATASTREAM_ID"
 		loop
 			if (NEW."PHENOMENON_TIME_START"<"DS_ROW"."PHENOMENON_TIME_START") then
-				update "DATASTREAMS" set "PHENOMENON_TIME_START" = NEW."PHENOMENON_TIME_START" where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+				queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = $1."PHENOMENON_TIME_START"';
+				delimitr := ',';
+			elseif (OLD."PHENOMENON_TIME_START" = "DS_ROW"."PHENOMENON_TIME_START") then
+				queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+				delimitr := ',';
 			end if;
 			if (coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") > "DS_ROW"."PHENOMENON_TIME_END") then
-				update "DATASTREAMS" set "PHENOMENON_TIME_END" = coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") where "DATASTREAMS"."ID" = "DS_ROW"."ID";
-			end if;
-
-			if (OLD."PHENOMENON_TIME_START" = "DS_ROW"."PHENOMENON_TIME_START"
-				or coalesce(OLD."PHENOMENON_TIME_END", OLD."PHENOMENON_TIME_START") = "DS_ROW"."PHENOMENON_TIME_END")
-			then
-				update "DATASTREAMS"
-					set "PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-					where "DATASTREAMS"."ID" = "DS_ROW"."ID";
-				update "DATASTREAMS"
-					set "PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-					where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+				queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = coalesce($1."PHENOMENON_TIME_END", $1."PHENOMENON_TIME_START")';
+				delimitr := ',';
+			elseif (coalesce(OLD."PHENOMENON_TIME_END", OLD."PHENOMENON_TIME_START") = "DS_ROW"."PHENOMENON_TIME_END") then
+				queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+				delimitr := ',';
 			end if;
 		end loop;
-		return NEW;
 	end if;
 
 
@@ -176,26 +203,23 @@ then
 		for "DS_ROW" in select * from "DATASTREAMS" where "ID"=NEW."DATASTREAM_ID"
 		loop
 			if (NEW."RESULT_TIME" < "DS_ROW"."RESULT_TIME_START") then
-				update "DATASTREAMS" set "RESULT_TIME_START" = NEW."RESULT_TIME" where "ID" = "DS_ROW"."ID";
+				queryset := queryset || delimitr || '"RESULT_TIME_START" = $1."RESULT_TIME"';
+				delimitr := ',';
+			elseif (OLD."RESULT_TIME" = "DS_ROW"."RESULT_TIME_START") then
+				queryset := queryset || delimitr || '"RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+				delimitr := ',';
 			end if;
 			if (NEW."RESULT_TIME" > "DS_ROW"."RESULT_TIME_END") then
-				update "DATASTREAMS" set "RESULT_TIME_END" = NEW."RESULT_TIME" where "ID" = "DS_ROW"."ID";
-			end if;
-
-			if (OLD."RESULT_TIME" = "DS_ROW"."RESULT_TIME_START")
-			then
-				update "DATASTREAMS"
-					set "RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-					where "DATASTREAMS"."ID" = "DS_ROW"."ID";
-			end if;
-			if (OLD."RESULT_TIME" = "DS_ROW"."RESULT_TIME_END")
-			then
-				update "DATASTREAMS"
-					set "RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-					where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+				queryset := queryset || delimitr || '"RESULT_TIME_END" = $1."RESULT_TIME"';
+				delimitr := ',';
+			elseif (OLD."RESULT_TIME" = "DS_ROW"."RESULT_TIME_END") then
+				queryset := queryset || delimitr || '"RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+				delimitr := ',';
 			end if;
 		end loop;
-		return NEW;
+	end if;
+	if (delimitr = ',') then
+		EXECUTE 'update "DATASTREAMS" SET ' || queryset ||  ' where "DATASTREAMS"."ID"=$1."DATASTREAM_ID"' using NEW;
 	end if;
 end if;
 
@@ -205,24 +229,19 @@ then
 		select * into "MDS_ROW" from "MULTI_DATASTREAMS" where "MULTI_DATASTREAMS"."ID"=NEW."MULTI_DATASTREAM_ID";
 		
 		if (NEW."PHENOMENON_TIME_START"<"MDS_ROW"."PHENOMENON_TIME_START") then
-			update "MULTI_DATASTREAMS" set "PHENOMENON_TIME_START" = NEW."PHENOMENON_TIME_START" where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+			queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = $1."PHENOMENON_TIME_START"';
+			delimitr := ',';
+		elseif (OLD."PHENOMENON_TIME_START" = "MDS_ROW"."PHENOMENON_TIME_START") then
+			queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+			delimitr := ',';
 		end if;
 		if (coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") > "MDS_ROW"."PHENOMENON_TIME_END") then
-			update "MULTI_DATASTREAMS" set "PHENOMENON_TIME_END" = coalesce(NEW."PHENOMENON_TIME_END", NEW."PHENOMENON_TIME_START") where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+			queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = coalesce($1."PHENOMENON_TIME_END", $1."PHENOMENON_TIME_START")';
+			delimitr := ',';
+		elseif (coalesce(OLD."PHENOMENON_TIME_END", OLD."PHENOMENON_TIME_START") = "MDS_ROW"."PHENOMENON_TIME_END") then
+			queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+			delimitr := ',';
 		end if;
-
-		if (OLD."PHENOMENON_TIME_START" = "MDS_ROW"."PHENOMENON_TIME_START"
-			or coalesce(OLD."PHENOMENON_TIME_END", OLD."PHENOMENON_TIME_START") = "MDS_ROW"."PHENOMENON_TIME_END")
-		then
-			update "MULTI_DATASTREAMS"
-				set "PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-				where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
-			update "MULTI_DATASTREAMS"
-				set "PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-				where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
-		end if;
-		
-		return NEW;
 	end if;
 
 
@@ -230,26 +249,23 @@ then
 		select * into "MDS_ROW" from "MULTI_DATASTREAMS" where "MULTI_DATASTREAMS"."ID"=NEW."MULTI_DATASTREAM_ID";
 		
 		if (NEW."RESULT_TIME" < "MDS_ROW"."RESULT_TIME_START") then
-			update "MULTI_DATASTREAMS" set "RESULT_TIME_START" = NEW."RESULT_TIME" where "ID" = "MDS_ROW"."ID";
+			queryset := queryset || delimitr || '"RESULT_TIME_START" = $1."RESULT_TIME"';
+			delimitr := ',';
+		elseif (OLD."RESULT_TIME" = "MDS_ROW"."RESULT_TIME_START") then
+			queryset := queryset || delimitr || '"RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+			delimitr := ',';
 		end if;
+
 		if (NEW."RESULT_TIME" > "MDS_ROW"."RESULT_TIME_END") then
-			update "MULTI_DATASTREAMS" set "RESULT_TIME_END" = NEW."RESULT_TIME" where "ID" = "MDS_ROW"."ID";
+			queryset := queryset || delimitr || '"RESULT_TIME_END" = $1."RESULT_TIME"';
+			delimitr := ',';
+		elseif (OLD."RESULT_TIME" = "MDS_ROW"."RESULT_TIME_END") then
+			queryset := queryset || delimitr || '"RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+			delimitr := ',';
 		end if;
-
-		if (OLD."RESULT_TIME" = "MDS_ROW"."RESULT_TIME_START")
-		then
-			update "MULTI_DATASTREAMS"
-				set "RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "MDS_ROW"."ID")
-				where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
-		end if;
-		if (OLD."RESULT_TIME" = "MDS_ROW"."RESULT_TIME_END")
-		then
-			update "MULTI_DATASTREAMS"
-				set "RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-				where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
-		end if;
-
-		return NEW;
+	end if;
+	if (delimitr = ',') then
+		EXECUTE 'update "MULTI_DATASTREAMS" SET ' || queryset ||  ' where "MULTI_DATASTREAMS"."ID"=$1."MULTI_DATASTREAM_ID"' using NEW;
 	end if;
 end if;
 
@@ -285,8 +301,10 @@ create or replace function datastreams_update_delete()
   returns trigger as
 $BODY$
 declare
-"DS_ROW" "DATASTREAMS"%rowtype;
-"MDS_ROW" "MULTI_DATASTREAMS"%rowtype;
+	"DS_ROW" "DATASTREAMS"%rowtype;
+	"MDS_ROW" "MULTI_DATASTREAMS"%rowtype;
+	queryset TEXT := '';
+	delimitr char(1) := ' ';
 begin
 
 if (OLD."DATASTREAM_ID" is not null) 
@@ -296,27 +314,24 @@ then
 	if (OLD."PHENOMENON_TIME_START" = "DS_ROW"."PHENOMENON_TIME_START"
 		or coalesce(OLD."PHENOMENON_TIME_END", OLD."PHENOMENON_TIME_START") = "DS_ROW"."PHENOMENON_TIME_END")
 	then
-		update "DATASTREAMS"
-			set "PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-			where "DATASTREAMS"."ID" = "DS_ROW"."ID";
-		update "DATASTREAMS"
-			set "PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-			where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+		delimitr := ',';
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
 	end if;
 
 	if (OLD."RESULT_TIME" = "DS_ROW"."RESULT_TIME_START")
 	then
-		update "DATASTREAMS"
-			set "RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-			where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+		queryset := queryset || delimitr || '"RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+		delimitr := ',';
 	end if;
 	if (OLD."RESULT_TIME" = "DS_ROW"."RESULT_TIME_END")
 	then
-		update "DATASTREAMS"
-			set "RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = "DS_ROW"."ID")
-			where "DATASTREAMS"."ID" = "DS_ROW"."ID";
+		queryset := queryset || delimitr || '"RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."DATASTREAM_ID" = $1."DATASTREAM_ID")';
+		delimitr := ',';
 	end if;
-
+	if (delimitr = ',') then
+		EXECUTE 'update "DATASTREAMS" SET ' || queryset ||  ' where "DATASTREAMS"."ID"=$1."DATASTREAM_ID"' using NEW;
+	end if;
 end if;	
 
 if (OLD."MULTI_DATASTREAM_ID" is not null) 
@@ -326,27 +341,24 @@ then
 	if (OLD."PHENOMENON_TIME_START" = "DS_ROW"."PHENOMENON_TIME_START"
 		or coalesce(OLD."PHENOMENON_TIME_END", OLD."PHENOMENON_TIME_START") = "MDS_ROW"."PHENOMENON_TIME_END")
 	then
-		update "MULTI_DATASTREAMS"
-			set "PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-			where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
-		update "MULTI_DATASTREAMS"
-			set "PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-			where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_START" = (select min("PHENOMENON_TIME_START") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+		delimitr := ',';
+		queryset := queryset || delimitr || '"PHENOMENON_TIME_END" = (select max(coalesce("PHENOMENON_TIME_END", "PHENOMENON_TIME_START")) from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
 	end if;
 
 	if (OLD."RESULT_TIME" = "MDS_ROW"."RESULT_TIME_START")
 	then
-		update "MULTI_DATASTREAMS"
-			set "RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-			where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+		queryset := queryset || delimitr || '"RESULT_TIME_START" = (select min("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+		delimitr := ',';
 	end if;
 	if (OLD."RESULT_TIME" = "MDS_ROW"."RESULT_TIME_END")
 	then
-		update "MULTI_DATASTREAMS"
-			set "RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = "MDS_ROW"."ID")
-			where "MULTI_DATASTREAMS"."ID" = "MDS_ROW"."ID";
+		queryset := queryset || delimitr || '"RESULT_TIME_END" = (select max("RESULT_TIME") from "OBSERVATIONS" where "OBSERVATIONS"."MULTI_DATASTREAM_ID" = $1."MULTI_DATASTREAM_ID")';
+		delimitr := ',';
 	end if;
-
+	if (delimitr = ',') then
+		EXECUTE 'update "MULTI_DATASTREAMS" SET ' || queryset ||  ' where "MULTI_DATASTREAMS"."ID"=$1."MULTI_DATASTREAM_ID"' using NEW;
+	end if;
 end if;	
 
 
