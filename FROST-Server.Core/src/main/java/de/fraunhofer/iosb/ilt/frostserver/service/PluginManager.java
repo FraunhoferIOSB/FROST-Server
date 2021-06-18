ee051a0fd12e1b7a54ca3dad9867225505abf232
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2021-06-18 10:11:58 +0200

    Improved error output when loading plugins

--- FROST-Server.Core/src/main/java/de/fraunhofer/iosb/ilt/frostserver/service/PluginManager.java
+++ FROST-Server.Core/src/main/java/de/fraunhofer/iosb/ilt/frostserver/service/PluginManager.java
@@ -158,8 +158,8 @@ public class PluginManager implements ConfigDefaults {
                     plugin.init(settings);
                 }
             } catch (NoClassDefFoundError | ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-                LOGGER.warn("Could not load given plugin class: '{}'", StringHelper.cleanForLogging(className));
-                LOGGER.debug("", ex);
+                LOGGER.warn("Could not load given plugin class: '{}': {}", StringHelper.cleanForLogging(className), ex.getMessage());
+                LOGGER.info("Exception:", ex);
             } catch (RuntimeException ex) {
                 LOGGER.warn("Plugin caused an exception during initialisation.", ex);
             }
