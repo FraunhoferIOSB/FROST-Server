78508ff3a9bc434f22d5324335950a2e0c076b37
Hylke van der Schaaf <hylke.vds@gmail.com>
2022-05-08 17:51:07 +0200

    [HELM] Synchronised helm with master branch

--- helm/upload.sh
+++ helm/upload.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 set -e
 
+# Setup SSH for GIT push
+mkdir -p ~/.ssh
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
 # release version
-if [[ "${GITHUB_BASE_REF}" == "" ]] && [[ "${GITHUB_REF}" == "/refs/tags"* ]]; then
+if [[ "${GITHUB_BASE_REF}" == "" ]] && [[ "${GITHUB_REF}" == "refs/tags"* ]]; then
+  echo -e "${HELM_SSH_KEY}" > ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
   cd helm-charts
   git add .
   git remote rm origin
-  git remote add origin https://phertweck:$GITHUB_API_KEY@github.com/FraunhoferIOSB/helm-charts
-  git commit -m "Travis build ${GITHUB_RUN_NUMBER} pushed"
+  git remote add origin git@github.com:FraunhoferIOSB/helm-charts.git
+  git commit -m "Github build ${GITHUB_RUN_NUMBER} pushed"
   git push origin master -fq
   cd ../
   rm -rf ./helm-charts
+  echo "Helm chart pushed to release repository"
 fi
 
 # Only deploy master branch and tagged builds to snapshot repository
-if [[ "${GITHUB_BASE_REF}" == "" ]] && ([[ "${GITHUB_REF}" == "/refs/heads/master" ]] || [[ "${GITHUB_REF}" == "/refs/tags"* ]]); then
+if [[ "${GITHUB_BASE_REF}" == "" ]] && ([[ "${GITHUB_REF}" == "refs/heads/master" ]] || [[ "${GITHUB_REF}" == "refs/tags"* ]]); then
+
+    echo -e "${HELM_SSH_KEY_SNAPSHOT}" > ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa
+
     cd helm-charts-snapshot
     git add .
     git remote rm origin
-    git remote add origin https://phertweck:$GITHUB_API_KEY@github.com/FraunhoferIOSB/helm-charts-snapshot
-    git commit -m "Travis build ${GITHUB_RUN_NUMBER} pushed"
+    git remote add origin git@github.com:FraunhoferIOSB/helm-charts-snapshot.git
+    git commit -m "Github build ${GITHUB_RUN_NUMBER} pushed"
     git push origin master -fq
     cd ../
     rm -rf ./helm-charts
+    echo "Helm chart pushed to snapshot repository"
 fi
 
-echo "Helm chart pushed"
+
