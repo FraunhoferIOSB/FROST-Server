4ad1eb48dd9bc7324c869903644f7fe52fdcb533
Hylke van der Schaaf <hylke.vds@gmail.com>
2023-02-10 17:43:01 +0100

    Updated ModelExtractor
    - Table-internal links
    - Multiple links between the same two tables

--- Tools/ModelExtractor/src/main/java/de/fraunhofer/iosb/ilt/frostserver/modelextractor/FXMLController.java
+++ Tools/ModelExtractor/src/main/java/de/fraunhofer/iosb/ilt/frostserver/modelextractor/FXMLController.java
@@ -48,11 +48,14 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.TreeMap;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -140,6 +143,8 @@ public class FXMLController implements Initializable {
     @FXML
     private TextField textFieldPassword;
     @FXML
+    private TextField textFieldSetPostfix;
+    @FXML
     private ListView<TableData> listViewFound;
     private ObservableList<TableData> tableList;
     @FXML
@@ -153,8 +158,17 @@ public class FXMLController implements Initializable {
 
     private ObjectMapper objectMapper;
 
+    private String setPostFix;
+
     private final Map<String, TableData> tables = new HashMap<>();
     private final Map<String, ConfigEditor<DefModel>> models = new TreeMap<>();
+
+    private static final Set<String> IGNORE_SCHEMAS = new HashSet<>();
+
+    static {
+        IGNORE_SCHEMAS.add("information_schema");
+        IGNORE_SCHEMAS.add("pg_catalog");
+    }
 
     private ObjectMapper getObjectMapper() {
         if (objectMapper == null) {
@@ -183,6 +197,7 @@ public class FXMLController implements Initializable {
 
     @FXML
     private void actionRead(ActionEvent event) {
+        setPostFix = textFieldSetPostfix.getText();
         readFromDatabase();
     }
 
@@ -271,15 +286,17 @@ public class FXMLController implements Initializable {
     static class TableData {
 
         private final String tableName;
-        private final Map<String, ForeignKeyData> refsToOther = new TreeMap<>();
-        private final Map<String, ForeignKeyData> refsFromOther = new TreeMap<>();
+        private final Map<String, List<ForeignKeyData>> refsToOther = new TreeMap<>();
+        private final Map<String, List<ForeignKeyData>> refsFromOther = new TreeMap<>();
         private final Map<String, FieldData> fields = new TreeMap<>();
         private int pkSize;
         private Node node;
         private TableDataController controller;
+        private String setPostFix;
 
-        public TableData(String tableName) {
+        public TableData(String tableName, String setPostFix) {
             this.tableName = tableName;
+            this.setPostFix = setPostFix;
             getNode();
         }
 
@@ -292,7 +309,7 @@ public class FXMLController implements Initializable {
                     controller.setTableName(tableName);
                     String prettyName = CaseUtils.toCamelCase(tableName, true, '_');
                     controller.setSingular(prettyName);
-                    controller.setPlural(prettyName + "Set");
+                    controller.setPlural(prettyName + setPostFix);
                 } catch (IOException ex) {
                     LOGGER.error("Failed to load FXML", ex);
                 }
@@ -329,11 +346,11 @@ public class FXMLController implements Initializable {
         }
 
         public void addReferenceToOther(ForeignKeyData fk) {
-            refsToOther.put(fk.otherTableName, fk);
+            refsToOther.computeIfAbsent(fk.otherTableName, t -> new ArrayList<>()).add(fk);
         }
 
         public void addReferenceFromOther(ForeignKeyData fk) {
-            refsFromOther.put(fk.myTableName, fk);
+            refsFromOther.computeIfAbsent(fk.myTableName, t -> new ArrayList<>()).add(fk);
         }
 
         public void setPkSize(int pkSize) {
@@ -383,7 +400,7 @@ public class FXMLController implements Initializable {
     }
 
     private TableData getTable(String tableName) {
-        return tables.computeIfAbsent(tableName, TableData::new);
+        return tables.computeIfAbsent(tableName, t -> new TableData(tableName, setPostFix));
     }
 
     private void readFromDatabase() {
@@ -397,6 +414,9 @@ public class FXMLController implements Initializable {
         Meta meta = dslContext.meta();
         List<Table<?>> dbTableList = meta.getTables();
         for (Table<?> table : dbTableList) {
+            if (IGNORE_SCHEMAS.contains(table.getSchema().getName())) {
+                continue;
+            }
             TableData tableData = getTable(table.getName());
             final UniqueKey primaryKey = table.getPrimaryKey();
             if (primaryKey != null) {
@@ -442,8 +462,10 @@ public class FXMLController implements Initializable {
                 LOGGER.info("    -> {}", fieldData);
             }
             LOGGER.info("  -> Relations");
-            for (ForeignKeyData fk : tableData.refsToOther.values()) {
-                LOGGER.info("    -> {}", fk);
+            for (List<ForeignKeyData> fkList : tableData.refsToOther.values()) {
+                for (ForeignKeyData fk : fkList) {
+                    LOGGER.info("    -> {}", fk);
+                }
             }
 
             tableData.analyse();
@@ -470,6 +492,136 @@ public class FXMLController implements Initializable {
         models.clear();
         Map<String, DefEntityType> entityTypes = new TreeMap<>();
         DefModel fullModel = new DefModel();
+
+        generateDataColumns(entityTypes, fullModel);
+
+        generateRelations(entityTypes);
+        for (DefEntityType defEt : fullModel.getEntityTypes()) {
+            models.put(defEt.getTable(), createEditorForModel(new DefModel().addEntityType(defEt)));
+        }
+    }
+
+    private void generateRelations(Map<String, DefEntityType> entityTypes) {
+        for (TableData tableData : tables.values()) {
+            if (!tableData.isEntityType()) {
+                continue;
+            }
+            // Direct links from this entity to other entities.
+            DefEntityType entityType = entityTypes.get(tableData.tableName);
+            generateRelationsDirect(tableData, entityTypes, entityType);
+            generateRelationsLinktable(tableData, entityTypes, entityType);
+        }
+    }
+
+    private void generateRelationsLinktable(TableData tableData, Map<String, DefEntityType> entityTypes, DefEntityType entityType) {
+        for (List<ForeignKeyData> fkList : tableData.refsFromOther.values()) {
+            if (fkList.size() == 2
+                    && fkList.get(0).myTableName.equals(fkList.get(1).myTableName)
+                    && fkList.get(0).otherTableName.equals(fkList.get(1).otherTableName)
+                    && fkList.get(0).fieldTheirs.name.equals(fkList.get(1).fieldTheirs.name)) {
+                generateSelfRelationLinkTable(fkList, entityType, tableData);
+            } else {
+                for (ForeignKeyData fk : fkList) {
+                    final TableData linkTableData = tables.get(fk.myTableName);
+                    if (linkTableData.isLinkTable()) {
+                        final String otherTableName = linkTableData.refsToOther.keySet().stream().findFirst().orElse("");
+                        final TableData otherTableData = tables.get(otherTableName);
+                        if (otherTableData == null) {
+                            LOGGER.warn("Ignoring fk over linkTable {}; No data on other table {}", linkTableData.tableName, otherTableName);
+                            continue;
+                        }
+                        if (otherTableData == tableData) {
+                            // Other side handles this link.
+                        } else {
+                            generateRelationLinkTable(linkTableData, otherTableName, entityTypes, otherTableData, fk, entityType);
+                        }
+                    } else {
+                        LOGGER.info("Ignoring fk from {} to {} ({}, not a link table)", tableData.tableName, linkTableData.tableName, linkTableData.getTableType());
+                    }
+                }
+            }
+        }
+    }
+
+    private void generateSelfRelationLinkTable(List<ForeignKeyData> fkList, DefEntityType entityType, TableData tableData) {
+        // This is a self-link
+        ForeignKeyData fk1 = fkList.get(0);
+        ForeignKeyData fk2 = fkList.get(1);
+        final TableData linkTableData = tables.get(fk1.myTableName);
+        DefNavigationProperty defNp = new DefNavigationProperty()
+                .setName(entityType.getPlural() + "_" + fk1.fieldMine.name)
+                .setEntityType(entityType.getName())
+                .setEntitySet(true)
+                .addHandler(new FieldMapperManyToMany()
+                        .setField(fk1.fieldTheirs.name)
+                        .setLinkTable(linkTableData.tableName)
+                        .setLinkOurField(fk1.fieldMine.name)
+                        .setLinkOtherField(fk2.fieldMine.name)
+                        .setOtherTable(tableData.tableName)
+                        .setOtherField(fk2.fieldTheirs.name))
+                .setInverse(new Inverse()
+                        .setName(entityType.getPlural() + "_" + fk2.fieldMine.name)
+                        .setEntitySet(true));
+        entityType.addNavigationProperty(defNp);
+    }
+
+    private void generateRelationLinkTable(final TableData linkTableData, final String otherTableName, Map<String, DefEntityType> entityTypes, final TableData otherTableData, ForeignKeyData fk, DefEntityType entityType) {
+        List<ForeignKeyData> fk2List = linkTableData.refsToOther.get(otherTableName);
+        if (fk2List.size() > 1) {
+            LOGGER.warn("Found multiple links from {} to link table {}", otherTableName, linkTableData.tableName);
+        }
+        final ForeignKeyData fk2 = fk2List.get(0);
+        DefEntityType otherEntityType = entityTypes.get(otherTableData.tableName);
+        DefNavigationProperty defNp = new DefNavigationProperty()
+                .setName(otherEntityType.getPlural())
+                .setEntityType(otherEntityType.getName())
+                .setEntitySet(true)
+                .addHandler(new FieldMapperManyToMany()
+                        .setField(fk.fieldTheirs.name)
+                        .setLinkTable(linkTableData.tableName)
+                        .setLinkOurField(fk.fieldMine.name)
+                        .setLinkOtherField(fk2.fieldMine.name)
+                        .setOtherTable(otherTableData.tableName)
+                        .setOtherField(fk2.fieldTheirs.name))
+                .setInverse(new Inverse()
+                        .setName(entityType.getPlural())
+                        .setEntitySet(true));
+        entityType.addNavigationProperty(defNp);
+    }
+
+    private void generateRelationsDirect(TableData tableData, Map<String, DefEntityType> entityTypes, DefEntityType entityType) {
+        for (List<ForeignKeyData> fkList : tableData.refsToOther.values()) {
+            for (ForeignKeyData fk : fkList) {
+                TableData otherTableData = tables.get(fk.otherTableName);
+                if (otherTableData.isEntityType()) {
+                    DefEntityType otherEntityType = entityTypes.get(otherTableData.tableName);
+                    String myRelationName = otherEntityType.getName();
+                    String otherRelationName = entityType.getPlural();
+                    if (entityType.equals(otherEntityType)) {
+                        myRelationName = otherEntityType.getName() + "Parent";
+                        otherRelationName = entityType.getPlural() + "Children";
+                    }
+                    DefNavigationProperty defNp = new DefNavigationProperty()
+                            .setName(myRelationName)
+                            .setEntityType(otherEntityType.getName())
+                            .setRequired(true)
+                            .addHandler(new FieldMapperOneToMany()
+                                    .setField(fk.fieldMine.name)
+                                    .setOtherField(fk.fieldTheirs.name)
+                                    .setOtherTable(fk.otherTableName))
+                            .setInverse(new Inverse()
+                                    .setName(otherRelationName)
+                                    .setEntitySet(true));
+                    entityType.addNavigationProperty(defNp);
+                } else {
+                    LOGGER.info("Ignoring fk from {} to {} ({})", tableData.tableName, otherTableData.tableName, otherTableData.getTableType());
+                }
+            }
+        }
+    }
+
+    private void generateDataColumns(Map<String, DefEntityType> entityTypes, DefModel fullModel) {
+        // First pass, gather non-fk columns.
         for (TableData tableData : tables.values()) {
             if (tableData.isEntityType()) {
                 DefEntityType defType = new DefEntityType()
@@ -491,64 +643,6 @@ public class FXMLController implements Initializable {
                 entityTypes.put(tableData.tableName, defType);
                 fullModel.addEntityType(defType);
             }
-        }
-        for (TableData tableData : tables.values()) {
-            if (tableData.isEntityType()) {
-                DefEntityType entityType = entityTypes.get(tableData.tableName);
-                for (ForeignKeyData fk : tableData.refsToOther.values()) {
-                    TableData otherTableData = tables.get(fk.otherTableName);
-                    if (otherTableData.isEntityType()) {
-                        DefEntityType otherEntityType = entityTypes.get(otherTableData.tableName);
-                        DefNavigationProperty defNp = new DefNavigationProperty()
-                                .setName(otherEntityType.getName())
-                                .setEntityType(otherEntityType.getName())
-                                .setRequired(true)
-                                .addHandler(new FieldMapperOneToMany()
-                                        .setField(fk.fieldMine.name)
-                                        .setOtherField(fk.fieldTheirs.name)
-                                        .setOtherTable(fk.otherTableName))
-                                .setInverse(new Inverse()
-                                        .setName(entityType.getPlural())
-                                        .setEntitySet(true));
-                        entityType.addNavigationProperty(defNp);
-                    } else {
-                        LOGGER.info("Ignoring fk from {} to {} ({})", tableData.tableName, otherTableData.tableName, otherTableData.getTableType());
-                    }
-                }
-                for (ForeignKeyData fk : tableData.refsFromOther.values()) {
-                    TableData otherTableData = tables.get(fk.myTableName);
-                    if (otherTableData.isLinkTable()) {
-                        TableData linkTableData = otherTableData;
-                        String otherTableName = linkTableData.refsToOther.keySet().stream().findFirst().orElse("");
-                        otherTableData = tables.get(otherTableName);
-                        if (otherTableData == null || otherTableData == tableData) {
-                            LOGGER.warn("Ignoring fk over linkTable for now {}, {}", linkTableData.tableName, otherTableName);
-                            continue;
-                        }
-                        ForeignKeyData fk2 = linkTableData.refsToOther.get(otherTableName);
-                        DefEntityType otherEntityType = entityTypes.get(otherTableData.tableName);
-                        DefNavigationProperty defNp = new DefNavigationProperty()
-                                .setName(otherEntityType.getPlural())
-                                .setEntityType(otherEntityType.getName())
-                                .addHandler(new FieldMapperManyToMany()
-                                        .setField(fk.fieldMine.name)
-                                        .setLinkTable(linkTableData.tableName)
-                                        .setLinkOurField(fk.fieldTheirs.name)
-                                        .setLinkOtherField(fk2.fieldTheirs.name)
-                                        .setOtherTable(otherTableData.tableName)
-                                        .setOtherField(fk2.fieldMine.name))
-                                .setInverse(new Inverse()
-                                        .setName(entityType.getPlural())
-                                        .setEntitySet(true));
-                        entityType.addNavigationProperty(defNp);
-                    } else {
-                        LOGGER.info("Ignoring fk from {} to {} ({})", tableData.tableName, otherTableData.tableName, otherTableData.getTableType());
-                    }
-                }
-            }
-        }
-        for (DefEntityType defEt : fullModel.getEntityTypes()) {
-            models.put(defEt.getTable(), createEditorForModel(new DefModel().addEntityType(defEt)));
         }
     }
 
