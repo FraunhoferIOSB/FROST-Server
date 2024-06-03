8ad787a157f455b859b96946974b74fb5fa1d431
Hylke van der Schaaf <hylke.vds@gmail.com>
2024-06-03 13:38:37 +0200

    Added initial Sampling extension

--- Plugins/ModelSampling/src/main/java/de/fraunhofer/iosb/ilt/frostserver/plugin/modelsampling/PluginModelSampling.java
+++ Plugins/ModelSampling/src/main/java/de/fraunhofer/iosb/ilt/frostserver/plugin/modelsampling/PluginModelSampling.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
+ * Copyright (C) 2024 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
  * Karlsruhe, Germany.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,9 +15,12 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.fraunhofer.iosb.ilt.frostserver.plugin.modelom;
+package de.fraunhofer.iosb.ilt.frostserver.plugin.modelsampling;
+
+import static de.fraunhofer.iosb.ilt.frostserver.service.InitResult.INIT_DELAY;
 
 import de.fraunhofer.iosb.ilt.frostserver.plugin.modelloader.PluginModelLoader;
+import de.fraunhofer.iosb.ilt.frostserver.service.InitResult;
 import de.fraunhofer.iosb.ilt.frostserver.service.Plugin;
 import de.fraunhofer.iosb.ilt.frostserver.service.PluginManager;
 import de.fraunhofer.iosb.ilt.frostserver.settings.ConfigDefaults;
@@ -28,38 +31,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Plugin loader for the Observations and Measurements Model plugin.
+ * Plugin loader for the Sampling Model plugin.
  */
-public class PluginModelOM implements Plugin, ConfigDefaults {
+public class PluginModelSampling implements Plugin, ConfigDefaults {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PluginModelOM.class.getName());
-    private static final String INIT_FAILED = "PluginModelLoader must be enabled and initialised before the ModelOM plugin!";
+    private static final Logger LOGGER = LoggerFactory.getLogger(PluginModelSampling.class.getName());
 
     @DefaultValueBoolean(false)
-    public static final String TAG_ENABLE_PDQ = "modelOM.enable";
+    public static final String TAG_ENABLE_PDQ = "modelSampling.enable";
 
     private boolean enabled;
 
-    public PluginModelOM() {
-        LOGGER.info("Creating new ModelOM Plugin.");
-    }
-
     @Override
-    public void init(CoreSettings settings) {
+    public InitResult init(CoreSettings settings) {
         Settings pluginSettings = settings.getPluginSettings();
-        enabled = pluginSettings.getBoolean(TAG_ENABLE_PDQ, PluginModelOM.class);
+        enabled = pluginSettings.getBoolean(TAG_ENABLE_PDQ, PluginModelSampling.class);
         if (enabled) {
             final PluginManager pluginManager = settings.getPluginManager();
-            pluginManager.registerPlugin(this);
             PluginModelLoader pml = pluginManager.getPlugin(PluginModelLoader.class);
             if (pml == null || !pml.isEnabled()) {
-                LOGGER.error(INIT_FAILED);
-                throw new IllegalArgumentException(INIT_FAILED);
+                LOGGER.warn("PluginModelLoader must be enabled first, delaying initialisation...");
+                return INIT_DELAY;
             }
-            pml.addLiquibaseFile("pluginmodelom/liquibase/tables.xml");
-            pml.addModelFile("pluginmodelom/model/Deployment.json");
-            pml.addModelFile("pluginmodelom/model/ObservingProcedure.json");
+            pml.addLiquibaseFile("pluginmodelsampling/liquibase/tables.xml");
+            pml.addModelFile("pluginmodelsampling/model/PreparationProcedure.json");
+            pml.addModelFile("pluginmodelsampling/model/PreparationStep.json");
+            pml.addModelFile("pluginmodelsampling/model/Sampler.json");
+            pml.addModelFile("pluginmodelsampling/model/Sampling.json");
+            pml.addModelFile("pluginmodelsampling/model/SamplingProcedure.json");
+            pluginManager.registerPlugin(this);
         }
+        return InitResult.INIT_OK;
     }
 
     @Override
