b316d30cb723868a54888b86d1c8ecb0a7a5d086
Hylke van der Schaaf <hylke.vds@gmail.com>
2024-06-03 10:52:16 +0200

    Database and Model fixes for v2 model

--- Plugins/CoreModelV2/src/main/java/de/fraunhofer/iosb/ilt/frostserver/plugin/coremodelv2/PluginCoreModelV2.java
+++ Plugins/CoreModelV2/src/main/java/de/fraunhofer/iosb/ilt/frostserver/plugin/coremodelv2/PluginCoreModelV2.java
@@ -17,7 +17,10 @@
  */
 package de.fraunhofer.iosb.ilt.frostserver.plugin.coremodelv2;
 
+import static de.fraunhofer.iosb.ilt.frostserver.service.InitResult.INIT_DELAY;
+
 import de.fraunhofer.iosb.ilt.frostserver.plugin.modelloader.PluginModelLoader;
+import de.fraunhofer.iosb.ilt.frostserver.service.InitResult;
 import de.fraunhofer.iosb.ilt.frostserver.service.Plugin;
 import de.fraunhofer.iosb.ilt.frostserver.service.PluginManager;
 import de.fraunhofer.iosb.ilt.frostserver.settings.ConfigDefaults;
@@ -34,28 +37,22 @@ import org.slf4j.LoggerFactory;
 public class PluginCoreModelV2 implements Plugin, ConfigDefaults {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PluginCoreModelV2.class.getName());
-    private static final String INIT_FAILED = "PluginModelLoader must be enabled and initialised before the CoreModelV2 plugin!";
 
     @DefaultValueBoolean(false)
     public static final String TAG_ENABLE_PCMV2 = "coreModelV2.enable";
 
     private boolean enabled;
 
-    public PluginCoreModelV2() {
-        LOGGER.info("Creating new CoreModelV2 Plugin.");
-    }
-
     @Override
-    public void init(CoreSettings settings) {
+    public InitResult init(CoreSettings settings) {
         Settings pluginSettings = settings.getPluginSettings();
         enabled = pluginSettings.getBoolean(TAG_ENABLE_PCMV2, PluginCoreModelV2.class);
         if (enabled) {
             final PluginManager pluginManager = settings.getPluginManager();
-            pluginManager.registerPlugin(this);
             PluginModelLoader pml = pluginManager.getPlugin(PluginModelLoader.class);
             if (pml == null || !pml.isEnabled()) {
-                LOGGER.error(INIT_FAILED);
-                throw new IllegalArgumentException(INIT_FAILED);
+                LOGGER.warn("PluginModelLoader must be enabled first, delaying initialisation...");
+                return INIT_DELAY;
             }
             pml.addLiquibaseFile("plugincoremodelv2/liquibase/tables.xml");
             pml.addModelFile("plugincoremodelv2/model/Datastream.json");
@@ -67,7 +64,9 @@ public class PluginCoreModelV2 implements Plugin, ConfigDefaults {
             pml.addModelFile("plugincoremodelv2/model/ObservedProperty.json");
             pml.addModelFile("plugincoremodelv2/model/Sensor.json");
             pml.addModelFile("plugincoremodelv2/model/Thing.json");
+            pluginManager.registerPlugin(this);
         }
+        return InitResult.INIT_OK;
     }
 
     @Override
