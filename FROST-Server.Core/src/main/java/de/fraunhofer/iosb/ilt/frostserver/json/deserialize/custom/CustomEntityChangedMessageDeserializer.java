043803739053b6d06446edd321c4aa15e1700b31
Hylke van der Schaaf <hylke.vds@gmail.com>
2026-03-12 08:53:34 +0100

    Reduced bus errors when receiving unknown entity types
    This can happen when one instance has a reduced data model.

--- FROST-Server.Core/src/main/java/de/fraunhofer/iosb/ilt/frostserver/json/deserialize/custom/CustomEntityChangedMessageDeserializer.java
+++ FROST-Server.Core/src/main/java/de/fraunhofer/iosb/ilt/frostserver/json/deserialize/custom/CustomEntityChangedMessageDeserializer.java
@@ -23,6 +23,7 @@ import de.fraunhofer.iosb.ilt.frostserver.model.ModelRegistry;
 import de.fraunhofer.iosb.ilt.frostserver.model.core.Entity;
 import de.fraunhofer.iosb.ilt.frostserver.property.EntityPropertyMain;
 import de.fraunhofer.iosb.ilt.frostserver.property.NavigationPropertyMain;
+import de.fraunhofer.iosb.ilt.frostserver.util.exception.UnknownEntityTypeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tools.jackson.core.JacksonException;
@@ -96,7 +97,8 @@ public class CustomEntityChangedMessageDeserializer extends ValueDeserializer<En
         final String typeString = parser.getValueAsString();
         EntityType type = modelRegistry.getEntityTypeForName(typeString, true);
         if (type == null) {
-            throw new IllegalArgumentException("Unknown EntityType: " + typeString);
+            LOGGER.info("Unknown EntityType: {}", typeString);
+            throw new UnknownEntityTypeException("Unknown EntityType: " + typeString);
         }
         if (entity != null) {
             entity.setEntityType(type);
